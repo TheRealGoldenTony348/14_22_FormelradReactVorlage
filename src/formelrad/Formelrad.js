c350b39432136aae0cee765e021a0e2e2f912ba3
TheRealGoldenTony348 <antoniwolski348@gmail.com>
2025-06-11 20:59:46 +0200

    code cleanup

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -26,10 +26,7 @@ export default function Formelrad() {
             /*calculate u and p */
             setValues(values => ({...values, u: values.i * values.r}));
             setValues(values => ({...values, p: values.i * values.i * values.r}));
-        }
-        event.preventDefault();
-        console.log("calculate");
-        if (values.i === "" && values.r === "") {
+        } else if (values.i === "" && values.r === "") {
             /*calculate i and r */
             setValues(values => ({...values, i: values.p / values.u}));
             setValues(values => ({...values, r: values.u * values.u / values.p}));
@@ -51,7 +48,6 @@ export default function Formelrad() {
                     <h2>Formelrad</h2>
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
-
                 <form onSubmit={handleSubmit}>
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
                     <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
